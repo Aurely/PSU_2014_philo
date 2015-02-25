5818f432a90141d6ee37137757cbf1433d97b522
lao_e <lao_e@epitech.eu>
2015-02-25 18:14:22 +0100

    changement radical

--- philosophes.c
+++ philosophes.c
@@ -5,7 +5,7 @@
 ** Login   <trotie_m@epitech.net>
 ** 
 ** Started on  Sun Feb 22 15:30:22 2015 Trotier Marie
-** Last update Tue Feb 24 11:55:17 2015 Trotier Marie
+** Last update Wed Feb 25 17:53:10 2015 Aurélie LAO
 */
 
 #include <stdlib.h>
@@ -13,78 +13,113 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <time.h>
+#include "philosophes.h"
 
 pthread_mutex_t m_creation = PTHREAD_MUTEX_INITIALIZER;
 
-void	*init(void *arg)
+/* void	*init(void *arg) */
+/* { */
+/*   int	*data; */
+
+/*   data = *(int **)arg; */
+/*   pthread_mutex_lock(&m_creation); */
+/*   data[1] = rand() % 3; */
+/*   printf("data[1] = %d\n", data[1]); */
+/*   pthread_mutex_unlock(&m_creation); */
+/* } */
+
+/* void		init_state(void *arg) */
+/* { */
+/*   int	*data; */
+/*   int	i; */
+
+/*   i = 0; */
+/*   data = *(int **)arg; */
+/*   while (i < 7) */
+/*     { */
+/*       if (data[i] == EAT) */
+/* 	func_eat(&data, i + 1, 3); */
+/*       else if (data[i] == THINK) */
+/* 	func_think(&data, i + 1); */
+/*       else */
+/* 	func_rest(&data, i + 1); */
+/*       i++; */
+/*     } */
+/* } */
+
+/* void		*create_philo() */
+/* { */
+/*   int		i; */
+/*   pthread_t	th; */
+/*   pthread_t	*t_philo; */
+/*   int		*rice; */
+/*   int		*data; */
+
+/*   i = 0; */
+/*   srand(time(0)); */
+/*   if (((t_philo = malloc(sizeof(pthread_t) * 7)) == NULL) || */
+/*       ((data = malloc(sizeof(int) * 7)) == NULL) || */
+/* -      ((rice = malloc(sizeof(int) * 7)) == NULL)) */
+/*     return; */
+/*   while (i < 7) */
+/*     { */
+/*       /\* pthread_create(&th, NULL, init, &data); *\/ */
+/*       rice[i] = 5; */
+/*       t_philo[i] = th; */
+/*       i++; */
+/*     } */
+/*   init_etat(&data); */
+/* } */
+
+void		*create_philo(void *arg)
 {
-  int	*data;
+  static int	a = 0;
+  int		b;
+  t_philo	*tmp;
 
-  data = *(int **)arg;
-  pthread_mutex_lock(&m_creation);
-  data[1] = rand() % 3;
-  printf("data[1] = %d\n", data[1]);
-  pthread_mutex_unlock(&m_creation);
-}
-
-void		init_state(void *arg)
-{
-  int	*data;
-  int	i;
-
-  i = 0;
-  data = *(int **)arg;
-  while (i < 7)
+  tmp = arg;
+  b = a;
+  a++;
+  if (pthread_mutex_trylock(*tmp->baguette[b]) != 0/* = Mutex unlock = Gauche libre*/)
     {
-      if (data[i] == EAT)
+      if (pthread_mutex_trylock(*tmp->baguette[a]) != 0/* = mutex unlock = Droite libre*/)
 	{
-	  func_eat(&data, i + 1, 3);
-	  /*RANDOM 0 || 1
-	    data[i + 1] = 0;REPOS;*/
-	}
-      else if (data[i] == THINK)
-	{
-	  func_think(&data, i + 1);
+	  
 	}
       else
 	{
-	  func_rest(&data, i + 1);
 	}
-      i++;
     }
-}
-
-void		*create_philo()
-{
-  int		i;
-  pthread_t	th;
-  pthread_t	*t_philo;
-  int		*rice;
-  int		*data;
-
-  i = 0;
-  srand(time(0));
-  if (((t_philo = malloc(sizeof(pthread_t) * 7)) == NULL) ||
-      ((data = malloc(sizeof(int) * 7)) == NULL) ||
--      ((rice = malloc(sizeof(int) * 7)) == NULL))
-    return;
-  while (i < 7)
+  else /* baguette de gauche prise*/
     {
-      pthread_create(&th, NULL, init, &data);
-      rice[i] = 5;
-      t_philo[i] = th;
-      i++;
+
     }
-  init_etat(&data);
+  sleep(1);
+  return (NULL);
 }
 
 int	main()
 {
-  //pthread_t     th_philo;
+  pthread_t     th_philo[PHIL];
+  t_philo	my_philo;
+  int		i;
 
-  //pthread_create(&th_philo, NULL, create_philo, NULL);
-  //pthread_join(th_philo, NULL);
-
-  create_philo();
+  i = 0;
+  while (i < PHIL)
+    {
+      my_philo.rice[i] = 5;
+      my_philo.state[i] = 'R';
+      my_philo.energy[i] = '1';
+      my_philo.baguette[i] = PTHREAD_MUTEX_INITIALIZER;
+      pthread_mutex_unlock(*my_philo.baguette[i]);
+      pthread_create(&th_philo[i], NULL, create_philo, &my_philo);
+      i++;
+    }
+  i = 0;
+  while (i < PHIL)
+    {
+      pthread_join(th_philo[i], NULL);
+      i = i + 1;
+    }
   return (0);
 }
--- philosophes.h
+++ philosophes.h
@@ -5,24 +5,23 @@
 ** Login   <trotie_m@epitech.net>
 ** 
 ** Started on  Sun Feb 22 18:19:08 2015 Trotier Marie
-** Last update Mon Feb 23 18:24:13 2015 Trotier Marie
+** Last update Wed Feb 25 18:04:17 2015 Aurélie LAO
 */
 
 #ifndef PHILOSOPHES_H_
 # define PHILOSOPHES_H_
 
-typedef struct	s_philo {
-  pthread_t	th_philo;  
-  char		*name;
-  int		rice;
+# define PHIL		7
+# define MAX_EAT	3
+# define MAX_THINK	3
+# define INIT_RICE	10
+
+typedef struct	s_philo
+{
+  pthread_mutex_t bagette[PHIL];
+  char		stat[PHIL];
+  int		rice[PHIL];
+  int		energy[PHIL];
 }		t_philo;
-
-
-typedef struct	s_state {
-  int		etat;
-}		t_state;
-
-#define true 1
-#define false 0
 
 #endif /* !PHILOSOPHES_H_ */
