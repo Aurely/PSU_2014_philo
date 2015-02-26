5880978e23dd4306ff2dd620c386a58cb33dee41
trotie_m <trotie_m@epitech.eu>
2015-02-26 20:12:17 +0100

    modifs

--- data.c
+++ data.c
@@ -5,7 +5,7 @@
 ** Login   <trotie_m@epitech.net>
 ** 
 ** Started on  Tue Feb 24 11:13:06 2015 Trotier Marie
-** Last update Thu Feb 26 15:39:11 2015 Aurélie LAO
+** Last update Thu Feb 26 19:40:21 2015 Trotier Marie
 */
 
 #include <stdlib.h>
@@ -14,19 +14,33 @@
 #include <pthread.h>
 #include <time.h>
 
-void		func_eat()
+void		func_eat(int philo, int next_philo, t_philo my_philo, int b)
 {
+  pthread_mutex_lock(&baguette);
+  my_philo.rice[philo] = my_philo.rice[philo] - 1;
+  printf("philo %d -> fonction ---> eat     | O |\n", b);
+  pthread_mutux_unlock(&baguette);
   /*
     lock ou unlock les mutex -> baguettes
     diminuer le nombre de riz
-    
    */
 }
 
-void		func_think()
+void		func_think(int philo, int next_philo, t_philo my_philo, int b)
 {
+  if (my_philo.think[philo] < MAX_THINK || my_philo.think[philo] == MAX-THINK)
+    func_rest(philo, next_philo, b);
+  else
+    {
+      pthread_mutex_lock(baguette[philo]);
+      printf("philo %d -> fonction ---> think     | O |\n", b);
+      my_philo.think[philo] = my_phuli.think[philo] + 1;
+      pthread_mutex_unlock(baguette[philo]);
+    }
 }
 
-void		func_rest()
-{    
+void		func_rest(int philo, int next_philo, int b)
+{
+  pthread_mutex_unlock(&baguette);
+  printf("philo %d -> fonction ---> rest     . O .\n", b);
 }
